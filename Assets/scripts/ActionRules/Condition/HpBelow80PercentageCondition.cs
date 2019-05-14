671b7450e4aa024a651a620d7f4173350f2e35c0
Wang Sheng <wang.sheng.unique@gmail.com>
2019-05-14 19:04:30 +0900

    added hp below 80% condition

---
+++ Assets/scripts/ActionRules/Condition/HpBelow80PercentageCondition.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using ActionRules;
+using Controllers;
+using UnityEngine;
+
+namespace Conditions
+{
+    public class HpBelow80PercentageCondition : Condition
+    {
+        public override bool evaluate(List<GameObject> targets, GameObject defaultTarget, GameObject self)
+        {
+            CharacterControl cc = self.GetComponent<CharacterControl>();
+            EvaluatedTarget = self;
+            return (float)cc.CharacterStatus.hp / cc.CharacterStatus.maxHp < 0.8f;
+        }
+        
+    }
+}
